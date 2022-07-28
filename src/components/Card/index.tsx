136e5c9aa599647c7756a65e3653f6c516abdc6d
Diogo Leite <diogoleite87@gmail.com>
2022-07-28 09:45:19 -0300

    update card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,20 +13,37 @@ function Card(props: IProps) {
 
     const {name, description, id, href, type} = props;
 
-    return (
-        <div className="container">
-
-            <div className="card">
-                <div className="content">
-                    <h2>{id}</h2>
-                    <h3>{name}</h3>
-                    <p>{description}</p>
-                    <a href={href} target="_blank">Mais Informações</a>
+    if (type == 0) {
+        return (
+            <div className="container">
+    
+                <div className="card-special">
+                    <div className="content">
+                        <h2>{id}</h2>
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <a href={href} target="_blank">Mais Informações</a>
+                    </div>
+    
                 </div>
-
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container">
+    
+                <div className="card">
+                    <div className="content">
+                        <h2>{id}</h2>
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <a href={href} target="_blank">Mais Informações</a>
+                    </div>
+    
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Card
